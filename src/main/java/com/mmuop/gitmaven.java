310564a9e9b0db2a9cc55cc5c4e960bfd974c848
mmuopQQ <1819469237@qq.com>
2022-11-25 16:09:58 +0800

    hot-fix first

--- src/main/java/com/mmuop/gitmaven.java
+++ src/main/java/com/mmuop/gitmaven.java
@@ -4,5 +4,6 @@ public class gitmaven {
     public static void main(String[] args) {
         System.out.println("sss");
         System.out.println("sss");
+        System.out.println("sss");
     }
 }
